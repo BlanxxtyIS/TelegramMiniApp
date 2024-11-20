4430315f4ba22312846318f7d4732643060f469d
MarlineIOS <blanxxtygadress@gmail.com>
2024-11-20 20:46:57 +0300

    Update deploy configurations

--- Program.cs
+++ Program.cs
@@ -7,9 +7,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Добавьте эту строку для правильной работы базового пути
+builder.HostEnvironment.BaseAddress = builder.HostEnvironment.BaseAddress.TrimEnd('/') + "/MarlineIOS/";
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<TelegramService>();
 
 await builder.Build().RunAsync();
-
-//commit
