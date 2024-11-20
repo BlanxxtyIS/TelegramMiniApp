da3b94c567b50ddb8bfedae763648c41a116d07a
MarlineIOS <blanxxtygadress@gmail.com>
2024-11-20 19:56:15 +0300

    commit

---
+++ Services/TelegramService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.JSInterop;
+
+namespace TelegramMiniApp.Services
+{
+    public class TelegramService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public TelegramService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _jsRuntime.InvokeVoidAsync("Telegram.WebApp.ready");
+        }
+
+        public async Task<string> GetUserName()
+        {
+            return await _jsRuntime.InvokeAsync<string>("eval", "window.Telegram.WebApp.initDataUnsafe.user.username");
+        }
+
+        public async Task ShowAlert(string message)
+        {
+            await _jsRuntime.InvokeVoidAsync("Telegram.WebApp.showAlert", message);
+        }
+    }
