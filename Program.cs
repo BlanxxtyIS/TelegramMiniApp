f526cbc4599c524352101d1a6ba2a9e84c255b69
Khasanov.MI <Khasanov.MI@D3072-203-SMV>
2024-11-21 08:36:38 +0300

    Тест для отображения NavMenu

--- Program.cs
+++ Program.cs
@@ -7,13 +7,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Правильный способ установки базового URL
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<TelegramService>();
 
-// Настройка базового пути для маршрутизации
-var baseAddress = new Uri(builder.HostEnvironment.BaseAddress);
-var basePath = baseAddress.PathAndQuery.TrimEnd('/');
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri($"{baseAddress.Scheme}://{baseAddress.Authority}{basePath}/") });
+
+//var baseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+//var basePath = baseAddress.PathAndQuery.TrimEnd('/');
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri($"{baseAddress.Scheme}://{baseAddress.Authority}{basePath}/") });
 
 await builder.Build().RunAsync();
